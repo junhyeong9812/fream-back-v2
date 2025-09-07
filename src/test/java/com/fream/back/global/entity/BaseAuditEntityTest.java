e89a220161afee6690f43d6d0ec3d4ee90ca6a3e
jun <pickjog@gmail.com>
2025-09-07 09:52:19 +0900

    create BaseAuditEntity

--- src/test/java/com/fream/back/global/entity/BaseAuditEntityTest.java
+++ src/test/java/com/fream/back/global/entity/BaseAuditEntityTest.java
@@ -1,8 +1,25 @@
 package com.fream.back.global.entity;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BaseAuditEntityTest {
 
+    static class TestAuditEntity extends BaseAuditEntity{
+    }
+
+    @Test
+    @DisplayName("BaseAuditEntity를 상송한 클래스를 생성할 수 있다.")
+    void canCreateAuditEntityClass(){
+        //given & when
+        TestAuditEntity entity = new TestAuditEntity();
+
+        //then
+        assertThat(entity).isNotNull();
+    }
 
 }
